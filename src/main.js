6e79dcc6d9634abf14d21a549ac7156377873308
Alexandr Vorobev <waalexandr11@yandex.ru>
2026-03-24 19:57:22 +0300

    prSeven

--- src/main.js
+++ src/main.js
@@ -16,7 +16,8 @@ import { initSearching } from './components/searching.js';
 
 
 // Исходные данные используемые в render()
-const {data, ...indexes} = initData(sourceData);
+//const {data, ...indexes} = initData(sourceData);
+const api = initData(sourceData);
 
 /**
  * Сбор и обработка полей из таблицы
@@ -47,18 +48,22 @@ function collectState() {
  * Перерисовка состояния таблицы при любых изменениях
  * @param {HTMLButtonElement?} action
  */
-function render(action) {
+async function render(action) {
     let state = collectState(); // состояние полей из таблицы
-    let result = [...data]; // копируем для последующего изменения
+    //let result = [...data]; // копируем для последующего изменения
+    let query = {};
 
     // @todo: использование
-
+    /*
     result = applySearching(result, state, action);
     result = applyFiltering(result, state, action);
     result = applySorting(result, state, action);
     result = applyPagination(result, state, action);
+    */
 
-    sampleTable.render(result)
+    const { total, items } = await api.getRecords(query);
+
+    sampleTable.render(items)
 }
 
 const sampleTable = initTable({
@@ -87,13 +92,18 @@ const applySorting = initSorting([        // Нам нужно передать 
     sampleTable.header.elements.sortByTotal
 ]);
 
+/*
 const applyFiltering = initFiltering(sampleTable.filter.elements, {    // передаём элементы фильтра
     searchBySeller: indexes.sellers                                    // для элемента с именем searchBySeller устанавливаем массив продавцов
 });
+*/
 
 const applySearching = initSearching("search");
 
 const appRoot = document.querySelector('#app');
 appRoot.appendChild(sampleTable.container);
 
-render();
+async function init(){
+const indexes = await api.getIndexes();
+}
+init().then(render);
